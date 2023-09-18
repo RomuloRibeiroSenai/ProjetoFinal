62d27ad2c82b378836faadfd7d0775c45aec97b9
RomuloRibeiroSenai <142239254+RomuloRibeiroSenai@users.noreply.github.com>
2023-09-18 11:22:15 -0300

    Add files via upload

---
+++ Banco/entities/src/main/java/com/example/Pessoas/Funcionario.java
@@ -0,0 +1,5 @@
+package Pessoas;
+
+public abstract class Funcionario {
+    
+}
---
+++ Banco/entities/src/main/java/com/example/Pessoas/Pessoa.java
@@ -0,0 +1,9 @@
+package Pessoas;
+
+public abstract class Pessoa {
+    
+    private String nome;
+    private String CPF;
+    private String data_nasc;
+    private UnidadeFederal UF;
+}
