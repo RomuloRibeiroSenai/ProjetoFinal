45d1f8199d5f108017a2e94dce515419fb3fc1dd
jose09812 <jose.f.santos22@aluno.senai.br>
2023-09-20 09:46:01 -0300

    Add menus

---
+++ banco/src/main/java/menus/Menu_funcionario.java
@@ -0,0 +1,12 @@
+package menus;
+
+public class Menu_funcionario {
+    
+    public static void menu_gerente(){
+        System.out.println("bem vindo menu gerente");
+    }
+    public static void menu_caixa(){
+        System.out.println("menu caixa");
+    }
+
+}
